12491c685b2be931ec7e02a0221992cd1797c81a
Kevin <kevinlee.3191@gmail.com>
2023-11-25 12:57:01 -0800

    double quotes to single

--- app/(site)/components/Main.tsx
+++ app/(site)/components/Main.tsx
@@ -1,15 +1,15 @@
-import TextAndStar from "@/components/TextAndStar";
+import TextAndStar from '@/components/TextAndStar';
 
 const expertise = [
-    {title: "Software Development", skills: "C#, C++, Python, Java, JavaScript, and OOP"},
-    {title: "Front End Development", skills: "HTML, CSS, JavaScript, React, Next.js, and Tailwind CSS"},
-    {title: "Back End Development", skills: "Node.js, REST APIs, ASP .NET, Django, and PostgreSQL"},
-    {title: "Game Development", skills: "C# with Unity and C++ with Unreal Engine 5"},
+    {title: 'Software Development', skills: 'C#, C++, Python, Java, JavaScript, and OOP'},
+    {title: 'Front End Development', skills: 'HTML, CSS, JavaScript, React, Next.js, and Tailwind CSS'},
+    {title: 'Back End Development', skills: 'Node.js, REST APIs, ASP .NET, Django, and PostgreSQL'},
+    {title: 'Game Development', skills: 'C# with Unity and C++ with Unreal Engine 5'},
 ]
 
 const Main = () => {
     return (
-        <div className="
+        <div className='
             hidden
             mt-[100px]
             md:flex
@@ -19,30 +19,30 @@ const Main = () => {
             items-center
             ml-[5vw]
             h-fit
-        ">
-            <h1 className="
+        '>
+            <h1 className='
                 text-[10em]
                 font-bold
                 text-white
-            ">
+            '>
                 Hi! I'm Kevin
             </h1>
-            <TextAndStar  text="FULL STACK SOFTWARE ENGINEER" title={false}/>
-            <h1 className="
+            <TextAndStar  text='FULL STACK SOFTWARE ENGINEER' title={false}/>
+            <h1 className='
                 text-[3em]
                 text-white
                 mt-[20px]
-            ">
+            '>
                 My Expertise
             </h1>
-            <div className="
+            <div className='
                 grid
                 md:grid-cols-4
                 md:gap-4
                 mt-[50px]
-            ">
+            '>
                 {expertise.map((items) => (
-                    <div key={items.title} className="
+                    <div key={items.title} className='
                         flex
                         flex-col
                         items-center
@@ -52,16 +52,16 @@ const Main = () => {
                         rounded-xl
                         p-6
                         text-center
-                    ">
-                        <h1 className="
+                    '>
+                        <h1 className='
                             text-4xl
                             font-bold
-                        ">
+                        '>
                             {items.title}
                         </h1>
-                        <h2 className="
+                        <h2 className='
                             text-3xl
-                        ">
+                        '>
                             {items.skills}
                         </h2>
                     </div>
